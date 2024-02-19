7a599bc79789d1156f879ea9f74a28d5fb69a2a6
Dimm4a <dimm4a@gmail.com>
2024-02-20 00:59:59 +0200

    feat: Lab2. Налагодження зв'язку з базою MySQL

--- lab1/settings.py
+++ lab1/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'lab1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        #'USER': 'admin',
+        #'PASSWORD': 'SQL_learning',
+        'USER': 'mysite_usr',
+        'PASSWORD': 'mysite_pass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
